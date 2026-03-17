70decb33334876b141b7c15acf140a74921d1117
BHOOMI SHIVHARE <152948254+shivharebhoomi07@users.noreply.github.com>
2026-03-17 07:20:36 +0000

    datatypes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3) // this returns 6 on browser console but here we are using nodejs, not browser so it will give an error
+
+let name="Bhoomi";
+let age=18;
+let isLogged=false;
+//let state,// this is undefined
+let state=null; //this means ki vo khaali h temperature logic
+
+//PRIMITIVE ONES
+//string
+//boolean
+//number
+//bigInt
+//null -standalone value
+//undefined 
+//symbol => unique
+
+
+//OBJECTS
+// type of null is an object but type of undefined is undefined
+
+
