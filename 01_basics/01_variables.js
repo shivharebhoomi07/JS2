32140af0a087fbb74ca7f7c0799e3559c4133d1c
BHOOMI SHIVHARE <152948254+shivharebhoomi07@users.noreply.github.com>
2026-03-17 07:21:14 +0000

    updated chnages

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=101;
+let accountEmail="bhoomi.com"
+var accountPassword="1234";
+accountCity="Jaipur"; 
+let accountState; //undefined
+//accountId=55;// not allowed
+accountEmail="xyz";
+accountPassword="007";
+accountCity="Bangalore";
+
+
+// console.log(accountId);
+console.table([accountEmail, accountId,accountPassword,accountState,accountCity])
