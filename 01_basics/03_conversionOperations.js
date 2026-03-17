62b28c8d1844a9d31f2cd19f17cb038cfeadc359
BHOOMI SHIVHARE <152948254+shivharebhoomi07@users.noreply.github.com>
2026-03-17 08:54:18 +0000

    conversion done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,18 @@
+let score="101aa";
+console.log(typeof score);
+
+let scoreInNumber= Number(score); //use to convert the type in number
+console.log(typeof scoreInNumber);
+console.log(scoreInNumber)
+
+//the number convert anything in number type no matter whats written inside it if it an actuall number it stays the number otherwise it becomes NaN
+//"33" => 33
+// "33abc" => NaN;
+// true=1; false=>0;
+
+
+//boolean conversion
+//1-true;
+//2-false;
+//""-false;
+//"hitesh"-true;
